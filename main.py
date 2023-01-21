4fec1b55c8cd20b2e6529b9944f9590358694f72
MeanTheBean <mdeancthemachine@gmail.com>
2023-01-21 17:51:45 +0000

    Removed useless code

--- main.py
+++ main.py
@@ -7,17 +7,15 @@ parser.add_argument("code_file", help="Prints the supplied argument.")
 args = parser.parse_args()
 
 f = open(args.code_file, "r")
-
 prog_text = f.readlines()
+f.close()
 
 checks.compCode(prog_text)
 
 error = checks.getErrors()
 
-out_text = checks.getOutput()
-
 if not error:
-    print(out_text)
+    print(checks.getOutput())
 else:
     print("ERROR: Unkown")
     quit()
