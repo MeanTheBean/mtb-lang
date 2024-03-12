a245b0735bc05f31f0c78ee9277d071e7cea3ddb
MeanTheBean <69492059+MeanTheBean@users.noreply.github.com>
2024-03-12 15:36:09 +0000

    Removed unused code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import checks
+import funcs
 
 parser = argparse.ArgumentParser(description='A program compiler')
 parser.add_argument("code_file", help="Prints the supplied argument.")
@@ -9,7 +10,10 @@ args = parser.parse_args()
 try:
   f = open(args.code_file, "r")
 except:
-  print(f"ERROR: No such file: {args.code_file}")
+  if args.code_file == "--version":
+    print(f"mtb-lang version {funcs.VERSION}; created by Mean The Bean 2024")
+  else:  
+    print(f"ERROR: No such file: {args.code_file}")
   quit()
 prog_text = f.readlines()
 f.close()
@@ -18,11 +22,3 @@ try:
   checks.compCode(prog_text)
 except:
   print("ERROR: Program stopped by user!")
-
-error = checks.getErrors()
-
-#if not error:
-#    print(checks.getOutput())
-#else:
-#    print("ERROR: Unkown")
-#    quit()
