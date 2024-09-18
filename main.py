82c98b23618499076638c3cde43156eeaa81f057
MeanTheBean <69492059+MeanTheBean@users.noreply.github.com>
2024-09-18 16:46:54 +0000

    added lib setup

--- main.py
+++ main.py
@@ -11,6 +11,22 @@ parser.add_argument("code_file", help="The file to run")
 args = parser.parse_args()
 
 try:
+  if os.name == "nt":
+    check_setup = Path("%localappdata%/.mtb/").exists()
+  elif os.name == "posix":
+    check_setup = os.path.exists("~/.mtb/")
+  #print(check_setup)
+  if not check_setup:
+    if os.name == "nt":
+      os.system("mkdir %localappdata%/.mtb/")
+      os.system("mkdir %localappdata%/.mtb/libs/")
+    elif os.name == "posix":
+      os.system("mkdir ~/.mtb/ > /dev/null 2>&1")
+      os.system("mkdir ~/.mtb/libs/ > /dev/null 2>&1")
+except:
+  print("WARNING: Could not create .mtb directory!")
+
+try:
   f = open(args.code_file, "r")
   file_dir = Path(args.code_file).parent.absolute()
   funcs.get_dir(str(file_dir))
